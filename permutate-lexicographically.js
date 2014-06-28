7ece75ab704a8d25340d9a50791eaa886a8ec623
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-29 07:12:59 +0800

    Updated permutateLexicographically sub module. Simplified permutateLexicographically procedure.

---
+++ permutate-lexicographically.js
@@ -0,0 +1,8 @@
+/*:
+*/
+var permutateLexicographically = function permutateLexicographically( startingIndex, endingIndex, dictionary, callback, separator ){
+
+};
+
+var work = require( "./work/work.js" );
+module.exports = permutateLexicographically;
