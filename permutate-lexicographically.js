857bf4ecee76aeab7f541e8c85c5060f9eff7700
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-07 12:50:31 +0800

    Added comments to permutateLexicographically procedure. Added compile-command sub module. Added execute-command sub module.

--- permutate-lexicographically.js
+++ permutate-lexicographically.js
@@ -1,4 +1,22 @@
 /*:
+    @module-configuration:
+        {
+            "packageName": "permutate-lexicographically",
+            "fileName": "permutate-lexicographically.js",
+            "moduleName": "permutateLexicographically",
+            "authorName": "Richeve S. Bebedor",
+            "authorEMail": "richeve.bebedor@gmail.com",
+            "repository": "git@github.com:volkovasystems/permutate-lexicographically.git",
+            "isGlobal": true
+        }
+    @end-module-configuration
+
+    @module-documentation:
+
+    @end-module-documentation
+
+    @include:
+    @end-include
 */
 var permutateLexicographically = function permutateLexicographically( startingIndex, endingIndex, dictionary, callback, separator ){
 
